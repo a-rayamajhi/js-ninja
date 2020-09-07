1bb2287f7846e25659533afd5a755feb4a89fea7
Anil Rayamajhi <anilrayamajhi@Anils-iMac.local>
2020-09-06 18:34:53 -0700

    Navigation in progress | data structure updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,24 +4,40 @@
 
   function AppViewModel() {
     var self = this;
-    self.routes = ["home", "trivia", "result", "mark-sheet"];
+    self.states = ["home", "trivia", "result", "mark-sheet"];
 
     // Place All Observables here
     self.jsNinja = ko.observable();
-    self.currentRoute = ko.observable();
-    self.currentTriviaIndex = ko.observable();
+    self.currentState = ko.observable();
+    self.currentTrivia = ko.observable();
+    self.chosenItem = ko.observable();
+    self.chosenItems = [];
+    self.length = 0;
 
-    self.toRouter = function (ctx, event) {
+    self.toState = function (ctx, event) {
+      console.log(self.length, self.chosenItems);
       if (!!event.target.dataset && !!event.target.dataset.value) {
         if (!!self.jsNinja()) {
-          self.currentRoute(event.target.dataset.value);
+          self.currentState(event.target.dataset.value);
+          self.currentTrivia(modal[self.length]);
+          self.length = self.length + 1;
         } else {
           alert("Please enter your name");
         }
       }
     };
 
-    self.currentRoute("home");
+    self.currentState("home");
+
+    self.chosenItem.subscribe(function (newValue) {
+      self.chosenItems.push(newValue);
+    });
+
+    window.onbeforeunload = function () {
+      if (self.currentState() !== "home") {
+        return true;
+      }
+    };
   }
 
   // knockout Binding
--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,11 +1,41 @@
 const modal = [
   {
+    id: "1",
     question: "Is Javascript an interpreted or compiled language?",
-    options: ["Interpreted", "Compiled", "Both", "Neither"],
+    options: [
+      {
+        name: "Interpreted",
+        isCorrect: true,
+      },
+      {
+        name: "Compiled",
+      },
+      {
+        name: "Both",
+      },
+      {
+        name: "Neither",
+      },
+    ],
   },
   {
+    id: "2",
     question:
       "Which Javascript data type is the variable in the example below? Example:\n<pre>var x = 3.14;<pre>",
-    options: ["Interpreted", "Compiled", "Both", "Neither"],
+    options: [
+      {
+        name: "Interpreted",
+        isCorrect: true,
+      },
+      {
+        name: "Compiled",
+      },
+      {
+        name: "Both",
+      },
+      {
+        name: "Neither",
+      },
+    ],
   },
 ];
