61e01912276ec21450316bcf0b46cdbadff5f9b3
Anil Rayamajhi <anilrayamajhi@Anils-iMac.local>
2020-09-13 19:05:28 -0700

    Result page in progress

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,24 @@
 (function () {
-  //See in Console; seed being loaded
-  var mo = modal.slice(0, 3);
-  console.log("MODAL:  ", modal, mo);
-
-  function randomizeTenQuestions(modal){
+  function randomizeTenQuestions(modal) {
     positions = [];
     questions = [];
     //create a list of position numbers equal to the number of questions in modal. Could be fixed at 30 but this is more flexible.
-    for(var i=0;i<modal.length;i++){
+    for (var i = 0; i < modal.length; i++) {
       positions.push(i);
     }
-    /* Iterate over the positions list 10 times. 
-    *  Pull a random number from the positions list. 
-    *  Use that number to copy a question obj with that index into questions arr. 
-    *  Eliminate that number from the pool to eliminate repeat picks.
-    */
-    for(var i=0;i<10;i++){
-      randomNum = positions[Math.floor(Math.random()*positions.length)];
+    /* Iterate over the positions list 10 times.
+     *  Pull a random number from the positions list.
+     *  Use that number to copy a question obj with that index into questions arr.
+     *  Eliminate that number from the pool to eliminate repeat picks.
+     */
+    for (var i = 0; i < 10; i++) {
+      randomNum = positions[Math.floor(Math.random() * positions.length)];
       questions.push(modal[randomNum]);
       index = positions.indexOf(randomNum);
-      positions.splice(index,1);
+      positions.splice(index, 1);
     }
     return questions;
   }
-  
-  console.log(randomizeTenQuestions(modal));
 
   function AppViewModel() {
     const self = this,
@@ -45,14 +39,17 @@
     self.chosenItem = ko.observable();
     self.chosenItems = [];
     self.length = ko.observable(0);
+    self.data = randomizeTenQuestions(modal);
+    self.score = ko.observable(0);
 
     self.toState = function (ctx, event) {
-      console.log(self.chosenItems);
       if (!!event.target.dataset && !!event.target.dataset.value) {
         if (!!self.jsNinja()) {
           if (self.length() >= COUNT) {
             if (event.target.dataset.value === "trivia") {
               self.currentState("result");
+
+              self.score(calculateScore());
               if (!!self.chosenItem()) {
                 self.chosenItems.push(self.chosenItem());
               }
@@ -64,10 +61,11 @@
               self.chosenItem(null);
               self.chosenItems = [];
               self.length(0);
+              self.data = randomizeTenQuestions(modal);
             }
           } else {
             self.currentState(event.target.dataset.value);
-            self.currentTrivia(mo[self.length()]);
+            self.currentTrivia(self.data[self.length()]);
             if (!!self.chosenItem()) {
               self.chosenItems.push(self.chosenItem());
             }
@@ -80,6 +78,11 @@
     };
 
     self.currentState("home");
+
+    function calculateScore() {
+      return self.chosenItems.filter((el) => el.hasOwnProperty("isCorrect"))
+        .length;
+    }
 
     window.onbeforeunload = function () {
       if (self.currentState() !== "home") {
